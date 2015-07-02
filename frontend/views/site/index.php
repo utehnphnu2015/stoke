39d59e8eafc454d095fce41b687f6a2667f968d7
p <p@inam>
2015-07-02 10:18:10 +0700

    เพิ่ม/แก้   บลาๆๆๆๆ

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,30 +1,25 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'STOKE';
 
 use yii\helpers\Url;
 
 ?>
 <div class="site-index">
+    
 
     <div class="jumbotron">
         <h2>ระบบบันทึกข้อมูลผู้ป่วย </h2><p>
-        <h3>STOKE ที่นอนโรงพยาบาล</h3>
-
+        <h3>STOKE ที่นอนโรงพยาบาล</h3><p>
+        <h2>จังหวัดพิษณุโลก</h2>
        
         <p><a class="btn btn-sm btn-success" href="<?php echo Url::to(['site/login']); ?>">เข้าใช้งาน คลิก!</a></p>
-        <p>
-        
-    </p>
+        <p>        
+        </p>
     </div>
-
     <div class="body-content">
-
-        
-
                 
             </div>
         </div>
-
     </div>
 </div>
