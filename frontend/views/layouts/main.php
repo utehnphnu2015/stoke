ed15950b15ecbc54f620433d0c5caef7fb9b39e5
p <p@inam>
2015-08-03 10:57:08 +0700

    เพิ่ม/แก้ บลาๆๆๆๆ

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'STOKE',
+                'brandLabel' => 'STROKE',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
